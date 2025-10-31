60a6cb84a891f29606d38410bc7070f0f7acc0cc
Maxime coux <100847666+mcoux@users.noreply.github.com>
2025-10-31 17:16:03 +0100

    prog faces

--- Assets/EXO_3.cs
+++ Assets/EXO_3.cs
@@ -26,39 +26,65 @@ public class EXO_3 : MonoBehaviour
 
     Mesh createSphere(float paralleles, float rayon, float meridiens, Vector3 centre)
     {
-        //if (meridiens < 3 || paralleles < 2)
-        //{
-        //    return null;
-        //}
+        if (meridiens < 3 || paralleles < 2)
+        {
+            return null;
+        }
 
         //System.Collections.Generic.List<Vector3> points = new();
         System.Collections.Generic.List<int> triangles = new();
 
-        //pointList.Add(new Vector3(centre.x, centre.y + rayon, centre.z));
+        pointList.Add(new Vector3(centre.x, centre.y + rayon, centre.z));
 
         Mesh mesh = new Mesh();
         Vector3 currentPoint;
         float meridianAngle = 2*Mathf.PI/meridiens;
         float parallAngle = Mathf.PI / (paralleles+1);
 
-        for (int i = 0; i < meridiens; i++) 
+        //Ajout des points
+        for (int j = 1; j < meridiens; j++) 
         {
-            Debug.Log(i);
 
-            for (int j = 1; j <= paralleles; j++)
+            for (int i = 0; i <= paralleles; i++)
             {
                 currentPoint = new Vector3(centre.x + Mathf.Cos(meridianAngle * i) * Mathf.Sin(parallAngle * j) , centre.y + Mathf.Cos(parallAngle * j), centre.z + Mathf.Sin(meridianAngle * i) * Mathf.Sin(parallAngle * j));
                 currentPoint *= rayon;
                 pointList.Add(currentPoint);
-                Debug.Log(currentPoint);
-                Debug.Log(parallAngle * j);
-                Debug.Log(j);
-
-
             }
         }
 
-        //pointList.Add(new Vector3(centre.x, centre.y - rayon, centre.z));
+        pointList.Add(new Vector3(centre.x, centre.y - rayon, centre.z));
+        mesh.vertices = pointList.ToArray();
+
+        triangles.Add(0);
+        triangles.Add(1);
+        triangles.Add((int)meridiens);
+
+        //Ajout des faces des poles
+        for (int i = 0;i<meridiens-1; i++)
+        {
+            triangles.Add(0);
+            triangles.Add(i+2);
+            triangles.Add(i + 1);
+
+
+            triangles.Add(pointList.Count - 1);
+            triangles.Add(pointList.Count - 2 - i);
+            triangles.Add(pointList.Count - 1 - i);
+
+        }
+
+        triangles.Add(7);
+        triangles.Add(6);
+        triangles.Add(4);
+
+        triangles.Add(7);
+        triangles.Add(4);
+        triangles.Add(5);
+
+
+        mesh.triangles = triangles.ToArray();
+        Debug.Log(mesh.triangles.Length);
 
         return mesh;
     }
